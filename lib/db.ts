a3ff21f94672571e59e1cfa2af4c039fbd5f0a67
fahrabyf200 <farhanf2003@gmail.com>
2026-04-17 03:05:25 +0700

    refactor: use environment variables for database

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,13 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: '127.0.0.1', // Pakai angka ini, jangan tulisan 'localhost'
-  user: 'root',
-  password: '', 
-  database: 'air_quality_db',
+  host: process.env.DB_HOST || '127.0.0.1', 
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '', 
+  database: process.env.DB_NAME || 'air_quality_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  enableKeepAlive: true, // Tambahkan ini agar koneksi tidak putus
+  enableKeepAlive: true,
   keepAliveInitialDelay: 0
 });
