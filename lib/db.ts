e5221bb3d1b36f54fa52969e9f99568fd13a60ab
fahrabyf200 <farhanf2003@gmail.com>
2026-04-17 04:39:11 +0700

    feat: integration with cloud database.

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,16 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1', 
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '', 
-  database: process.env.DB_NAME || 'air_quality_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+  // WAJIB UNTUK CLOUD DATABASE
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
